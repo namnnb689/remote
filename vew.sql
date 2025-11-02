cb076e910ef4e36b7fd76d6b972c3b983bcad1cc
namnnb47 <namnnb47@gmail.com>
2025-11-03 00:19:19 +0700

    update view

--- vew.sql
+++ vew.sql
@@ -1,11 +1,9 @@
-
 DROP MATERIALIZED VIEW IF EXISTS public.mv_assessment_attr_7days;
 
 CREATE MATERIALIZED VIEW public.mv_assessment_attr_7days AS
 SELECT *
 FROM vit_trans.assessment_attr
 WHERE date_time_last_modified >= NOW() - INTERVAL '7 days';
-
 
 CREATE INDEX idx_mv_assessment_attr_watermark
     ON public.mv_assessment_attr_7days (date_time_last_modified);
@@ -15,6 +13,6 @@ CREATE EXTENSION IF NOT EXISTS pg_cron;
 
 SELECT cron.schedule(
     'refresh_mv_assessment_attr_7days',
-    '0 16 * * *',  -- 16:00 UTC = 23:00 HKT
+    '0 16 * * *',  
     $$REFRESH MATERIALIZED VIEW CONCURRENTLY public.mv_assessment_attr_7days;$$
 );
